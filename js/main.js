562c7202331dc5820ecc9a2f773ce5102c706a03
Brian Hernandez <hernandezbriannnn@gmail.com>
2022-06-19 16:29:03 -0700

    90% done. Need to add random image function

--- js/main.js
+++ js/main.js
@@ -4,26 +4,37 @@ const button = document.querySelector('button');
 
 const replace = document.getElementById('question');
 
-const eightballImage = () => {
-    // add the random images here (currently this part not workin below)
-    let images = ["magic8ball_3.png", "magic8ball_15.png"];
-    let imagesLength =  images.length;
-    let randomNumber = Math.random();
-    randomNumber = randomNumber * imagesLength;
-    randomNumber = Math.floor(randomNumber);
-    let chosenImage = images[randomNumber];
-    answers.src = chosenImage;
+const replacetwo = document.getElementById('answers');
+
+
+
+    
+
+
+
+const crtImg = () => {
+
+    let image = document.createElement('img');
+    image.src = ['./img/magic8ball_3.png'/*, './img/magic8ball_4.png', './img/magic8ball_5.png', './img/magic8ball_4.png', './img/magic8ball_5.png', './img/magic8ball_6.png', './img/magic8ball_7.png', './img/magic8ball_8.png', './img/magic8ball_9.png', './img/magic8ball_10.png', './img/magic8ball_11.png', './img/magic8ball_12.png', './img/magic8ball_13.png', './img/magic8ball_14.png', './img/magic8ball_15.png', './img/magic8ball_16.png', './img/magic8ball_17.png', './img/magic8ball_18.png', './img/magic8ball_19.png', './img/magic8ball_20.png' */];
+    replacetwo.appendChild(image);
+    image.style.width = ('28rem')
+    
 }
 
+
 button.addEventListener('click', function() {
-    eightballImage();
+    
     replace.innerHTML = 
     `
-    <h1>${inputval.value}</h1>
+    <h1 style="text-align: center; padding-top: 20vh;">${inputval.value}</h1>
     `
     ;
+    crtImg();
+
+    
     
 });
+
 
 
 
