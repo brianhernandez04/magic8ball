c965c8fe4a970c072a8f6afce3cbab289e06da66
Brian Hernandez <hernandezbriannnn@gmail.com>
2022-06-19 13:46:01 -0700

    testing

--- js/main.js
+++ js/main.js
@@ -4,21 +4,25 @@ const button = document.querySelector('button');
 
 const replace = document.getElementById('question');
 
+const eightballImage = () => {
+    // add the random images here (currently this part not workin below)
+    let images = ["magic8ball_3.png", "magic8ball_15.png"];
+    let imagesLength =  images.length;
+    let randomNumber = Math.random();
+    randomNumber = randomNumber * imagesLength;
+    randomNumber = Math.floor(randomNumber);
+    let chosenImage = images[randomNumber];
+    answers.src = chosenImage;
+}
+
 button.addEventListener('click', function() {
+    eightballImage();
     replace.innerHTML = 
     `
     <h1>${inputval.value}</h1>
     `
     ;
-    // add the random images here
-    window.onload = function() {
-        var elem = document.createElement("img");
-        elem.setAttribute("src", "magic8ball_3.png");
-        elem.setAttribute("height", "40vh");
-        elem.setAttribute("width", "20rem");
-        elem.setAttribute("alt", "Random 8ball answers");
-        document.getElementById("answers").appendChild(elem);
-      }
+    
 });
 
 
